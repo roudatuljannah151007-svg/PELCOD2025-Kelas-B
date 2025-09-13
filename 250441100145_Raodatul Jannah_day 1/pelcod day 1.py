be89ab1e669c5dfe8058cf5e8dee1ec1ccf8cf5e
roudatuljannah151007-svg <roudatuljannah151007@gmail.com>
2025-09-13 12:15:46 +0700

    250441100145_RaodatulJannah_day1

---
+++ 250441100145_Raodatul Jannah_day 1/pelcod day 1.py
@@ -0,0 +1,29 @@
+# # nama = "Raodatul Jannah"
+# # NIM = 250441100145
+# # semester = 1
+
+# # print("=====BIODATA MAHASISWA=====")
+# # print("hallo, nama saya : "+ nama + "\n")
+# # print(f"NIM saya : {NIM}\n")
+# # print("saya berkuliah di semester : {}".format (semester))
+
+# a = 10
+# b = 5
+# c = a + b 
+# d = a - b
+# e = a * b
+# f = a / b
+# g = a % b 
+# print("Hasil Penjumlahan:", c)
+# print("Hasil Pengurangan:", d)
+# print("Hasil Perkalian:", e)
+# print("Hasil Pembagian:", f)
+# print("Hasil Sisa Bagi:", g)
+
+nama = input("Masukkan nama anda: ")
+umur = input("Masukkan umur anda: ")
+buah = input("Masukkan buah favorit Anda: ")
+
+print("Nama saya adalah " + nama)
+print("Umur saya adalah " + umur + " tahun")
+print("Buah favorit saya adalah " + buah)
